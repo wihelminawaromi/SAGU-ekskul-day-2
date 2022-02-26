c381a3decc0ebde2fe9ab035d6517bfc596ad658
Wihelmina_Waromi <waromiwihelmina@gmail.com>
2022-02-26 11:33:19 +0900

    push project03

---
+++ project03/comments.php
@@ -0,0 +1,14 @@
+<?php
+    //this is my first single line comment in php
+    echo "single line comment using '//' <br><br>";
+
+    #this also a 
+    #single line comment
+    echo "single line comment using '#'"
+
+    /* this is 
+    multiple line 
+    comment in PHP 
+    */
+    echo "multiple line comment in php using '/*...*/' <br><br> ";
+?>

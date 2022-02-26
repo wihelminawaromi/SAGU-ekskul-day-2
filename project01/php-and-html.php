4f741285f66c77cca9063adfa2c8567782a30e84
Wihelmina_Waromi <waromiwihelmina@gmail.com>
2022-02-26 10:49:57 +0900

    project 01

---
+++ project01/php-and-html.php
@@ -0,0 +1,18 @@
+<?php 
+    print("
+    <html>
+
+        <head>
+        
+             <title> PHP anf HTML </title>
+        </head>
+
+        <body>
+            <h1> Hello SAGU! </h1>
+            <p> We are Learning PHP Codes </p>
+        </>body
+
+    </html>
+
+    ");
+?>
